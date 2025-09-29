3908d44e87a0df4c430af2cfa3ee92a860b272cb
obb8923 <obb8923@naver.com>
2025-09-29 17:33:29 +0900

    .

--- src/domain/profile/screen/MyInfoScreen.tsx
+++ src/domain/profile/screen/MyInfoScreen.tsx
@@ -320,6 +320,7 @@ export const MyInfoScreen = () => {
               value={marketingConsent}
               onValueChange={setMarketingConsent}
               trackColor={{ false: Colors.gray600, true: Colors.primary }}
+              thumbColor={Colors.white}
             />
           </View>
 
